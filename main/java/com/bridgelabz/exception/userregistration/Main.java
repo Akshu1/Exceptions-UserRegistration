ec1d17e79e8e3933cd1cc9532da7f444ca090159
Akshatha <49878763+Akshu1@users.noreply.github.com>
2022-12-24 16:46:12 +0530

    Update Main.java

--- main/java/com/bridgelabz/exception/userregistration/Main.java
+++ main/java/com/bridgelabz/exception/userregistration/Main.java
@@ -2,6 +2,6 @@ package com.bridgelabz.exception.userregistration;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to User registration");
+        System.out.println("Welcome to welcome to User registration and Email Pattern Validation using Exception");
     }
 }
